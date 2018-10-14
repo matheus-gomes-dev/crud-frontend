8458422d1d5219b4cebfcedf4be0d293a053a159
matheus-lima92 <lima.eca@gmail.com>
2018-10-14 17:49:51 -0300

    initial structure

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,28 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <button className="btn btn-primary">teste</button>
+      </div>
+    );
+  }
+}
